391a05c8fe0ad19770d698816c9567e3228af0f7
faithyl <faithyl@yahoo-inc.com>
2014-10-10 13:25:12 +0800

    2nd Check-in

---
+++ SoMovie/SoMovie/FandangoClient.swift
@@ -0,0 +1,13 @@
+//
+//  FandangoClient.swift
+//  SoMovie
+//
+//  Created by Faith Cox on 10/10/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+import UIKit
+
+class FandangoClient: BDBOAuth1RequestOperationManager {
+   
+}
---
+++ SoMovie/SoMovie/RottenClient.swift
@@ -0,0 +1,13 @@
+//
+//  RottenClient.swift
+//  SoMovie
+//
+//  Created by Faith Cox on 10/10/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+import UIKit
+
+class RottenClient: BDBOAuth1RequestOperationManager {
+   
+}
